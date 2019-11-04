960196e6929d9cd7c25506f29b0605392f6f1ad9
sunecosuri <sunecosuri@gmail.com>
2019-11-04 22:08:17 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,39 @@
-# nodebrew
-set -x PATH $HOME/.nodebrew/current/bin $PATH
+set -x FZF_DEFAULT_OPTS '--height 50% --reverse --border'
 
-set -x GOPATH $HOME/.work/
+# GO
+set -x GOPATH $HOME/.work
 set -x PATH $GOPATH/bin $PATH
+set -x GOBIN $GOPATH/bin
 
-function fzf_select_repository
-    ghq list | fzf | read repo
-    [ -n "$repo" ]; or return 1
-    cd (ghq root)/$repo
-    commandline -f repaint
+# starship
+eval (starship init fish)
+
+# direnv
+eval (direnv hook fish)
+
+# anyenv
+status --is-interactive; and source (anyenv init -|psub)
+set -x PATH $HOME/.anyenv/bin $PATH
+
+# aws-cli
+complete -c aws -f -a '(begin; set -lx COMP_SHELL fish; set -lx COMP_LINE (commandline); /usr/local/bin/aws_completer; end)'
+
+# alias
+alias sudo 'sudo -E'
+alias nr 'npm run'
+alias ls 'exa'
+alias ssh 'ssh -A'
+alias cat 'bat'
+set -g segment_separator "$segment_separator\n"
+
+# func
+function r
+  ghq list | fzf | read repo && cd (ghq root)/$repo
 end
 
-function fzf_select_history
+function h
     history | fzf | read cmd
     commandline $cmd
     commandline -f repaint
 end
 
-function fish_user_key_bindings
-    bind \cf 'fzf_select_repository'
-    bind \cr 'fzf_select_history'
-end
-
-alias sudo 'sudo -E'
