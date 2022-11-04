558f086dc93f03ab38400e5dc95ba3d490370a8d
Kouki Narumi <sunecosuri@gmail.com>
2022-11-05 05:46:34 +0900

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 set -x FZF_DEFAULT_OPTS '--height 50% --reverse --border'
 
 # GO
@@ -5,15 +9,19 @@ set -x GOPATH $HOME/.work
 set -x PATH $GOPATH/bin $PATH
 set -x GOBIN $GOPATH/bin
 
+# Rust
+set -U fish_user_paths $fish_user_paths $HOME/.cargo/bin
+
 # starship
-eval (starship init fish)
+starship init fish | source
 
 # direnv
-eval (direnv hook fish)
+# eval (direnv hook fish)
 
-# anyenv
-status --is-interactive; and source (anyenv init -|psub)
-set -x PATH $HOME/.anyenv/bin $PATH
+if not test (asdf which node)
+  asdf plugin add nodejs;
+end
+source /usr/local/opt/asdf/asdf.fish
 
 # aws-cli
 complete -c aws -f -a '(begin; set -lx COMP_SHELL fish; set -lx COMP_LINE (commandline); /usr/local/bin/aws_completer; end)'
@@ -23,8 +31,16 @@ alias sudo 'sudo -E'
 alias nr 'npm run'
 alias ls 'exa'
 alias ssh 'ssh -A'
-alias cat 'bat'
+# alias cat 'bat'
 set -g segment_separator "$segment_separator\n"
+
+## git alias
+alias gco 'git checkout'
+alias grh 'git reset --hard'
+alias grs 'git reset --soft'
+alias gp 'git pull'
+alias gst 'git stash'
+alias gf 'git fetch'
 
 # func
 function r
@@ -32,8 +48,28 @@ function r
 end
 
 function h
-    history | fzf | read cmd
-    commandline $cmd
-    commandline -f repaint
+  history | fzf | read cmd
+  commandline $cmd
+  commandline -f repaint
 end
 
+function cb
+  eval "git branch --sort=-authordate | fzf --query (commandline)" | read -z select
+  if not test -z $select
+    git switch $select
+  end
+  commandline -f repaint
+end
+
+function togif
+  if count $argv = 2
+    ffmpeg -i $argv[1] -fs 9M -r 5 $argv[2]
+    return 1
+  else
+    echo 'USAGE: togif [INPUT] [OUTPUT]'
+  end
+end
+
+
+source /usr/local/opt/asdf/asdf.fish
+starship init fish | source
